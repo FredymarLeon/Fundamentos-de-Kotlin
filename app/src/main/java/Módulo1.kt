5593ce9275035183f46afcf6049e9ae525691121
FredymarLeon <fredymar@gmail.com>
2024-10-10 21:40:11 -0300

    Muito estudo...

---
+++ app/src/main/java/Módulo1.kt
@@ -0,0 +1,57 @@
+fun main() {
+    //Números Vs Textos
+//    val price = 31.90
+//    val resposta = price.toInt() //Entrada vai receber o valor de price e converter para string.
+//    println(resposta)
+
+//    val numero = "10"
+//    val resposta = numero.toString()
+//    println(resposta)
+
+//Desafio 1. Somar um número inteiro e um texto que representa um número. Avariável resposta deve ter o valor: 35
+
+    //solução 1
+//    val numero1: Int = 20
+//    val numero2: String = "15"
+//    val resposta2 = numero2.toInt()
+//    val resposta = numero1 + resposta2
+//    println(resposta)
+//
+//    //Solução 2.
+//    val numero1 = 25.5
+//    val numero2 = "9.5"
+//    val resposta2 = numero2.toDouble()
+//    val soma = numero1.plus(resposta2)
+//    val resultado = soma.toInt()
+//    println(resultado)
+
+    //Operadores
+//    var idade = 31
+//    idade = idade + 10
+//    idade = idade - 10
+////    idade = idade / 10
+//    idade = idade * 10
+//    println(idade)
+
+//    var textoPreco = "28"
+//    println(textoPreco + " é o preço.") //concatenando
+
+//    val acertou = false
+//    //true false | (1 - 0)
+//    println(!acertou) // is not - negação
+//
+//
+//    val nombre = "Fredymar"
+//    val apellido = "León"
+//    val primeraLetraDelNombre = 'F'
+//    val saltarLinea = '\n'
+//
+//    println(nombre + saltarLinea + apellido + saltarLinea + primeraLetraDelNombre)
+//    println(nombre.length)//ACESSAR A PROPRIEDADE DE UM OBJETO (STRING)
+//
+//    println(nombre.uppercase())//FUNÇÃO QUE MUDA O COMPORTAMENTO DE UM OBJETO (STRING)
+//    println(apellido.lowercase())
+
+
+
+}
